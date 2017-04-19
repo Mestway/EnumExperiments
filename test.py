2dd253958f84fbee0deb979722b89f4be48e61a4
Chenglong <mestway@gmail.com>
2017-04-19 00:02:49 -0700

    test file

--- test.py
+++ test.py
@@ -7,22 +7,25 @@ from subprocess32 import check_output, PIPE, TimeoutExpired
 
 import re
 
-test_dir = "data/dev_set"
+test_dir = "data/"
 dylib_location = "lib"
 jar_path = "out/artifacts/EnumExperiment_jar/EnumExperiment.jar"
 
+
 #java -Djava.library.path=lib/ -jar out/artifacts/EnumExperiment_jar/EnumExperiment.jar
 
-def main():
+def main(search_depth, complex_query_depth):
   print "file, collected, visited, time"
   for root, dirs, filenames in os.walk(test_dir):
     for f in filenames:
+      if ("X" in f) or ("R" in f):
+        continue
       target_file = os.path.join(root, f)
-      print f, ",",'%s' % ', '.join(map(str, run(target_file, "-a", str(1), str(0))))
+      print os.path.join(root, f), ",",'%s' % ', '.join(map(str, run(target_file, "-a", search_depth, complex_query_depth)))
 
 def run(target_file, pruning_strategy, max_depth, complex_query_depth):
   try:
-    command = ["java", "-Djava.library.path=" + dylib_location ,"-jar", jar_path, target_file, "-d", max_depth, pruning_strategy, "--complex-aggr-depth", complex_query_depth]
+    command = ["java", "-Djava.library.path=" + dylib_location ,"-jar", jar_path, target_file, "-d", str(max_depth), pruning_strategy, "--complex-aggr-depth", str(complex_query_depth)]
     output = check_output(command, stdin=PIPE, stderr=PIPE, timeout=300)
 
     for s in output.splitlines():
@@ -38,4 +41,4 @@ def run(target_file, pruning_strategy, max_depth, complex_query_depth):
     return [-1, -1, -1]
 
 if __name__ == '__main__':
-  main()
+  main(sys.argv[1], sys.argv[2])
